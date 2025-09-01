56b1d19b2e3ab310795960628f404010c09be969
AlphaBeast97 <ksaad5272@gmail.com>
2025-09-01 22:19:48 +0500

    feat: added companions Card on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,42 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import CallToAction from "@/components/CallToAction";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to SaaS App</h1>
-      <Button>
-        Lets Get Started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id='1'
+          name='Neura the Brainy explorer'
+          topic='Neural network of the brain'
+          subject='Neuroscience'
+          duration={45}
+          color='#ffda6e'
+        />
+        <CompanionCard
+          id='2'
+          name='Countsy the number wizard'
+          topic='Derivatives & Integral'
+          subject='Mathematics'
+          duration={30}
+          color='#e5d0ff'
+        />
+        <CompanionCard
+          id='3'
+          name='Verba the vocabulary builder'
+          topic='English lecture'
+          subject='Language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CallToAction />
+      </section>
+    </main>
   );
 };
 

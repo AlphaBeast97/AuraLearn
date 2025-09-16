3637edb7fbcc60c97935d3ae7344f18726da23bb
AlphaBeast97 <ksaad5272@gmail.com>
2025-09-16 14:50:03 +0500

    fix: correct route protection logic in middleware

--- src/middelware.ts
+++ src/middelware.ts
@@ -9,8 +9,8 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isProtectedRoute(req)) {
-    await auth.protect();
+  if (isProtectedRoute(req)) {
+    auth.protect();
   }
 });
 
